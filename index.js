7bf4c22e7fc06126cad9e6b90dcf08543275e62b
alexandre gomes <alecxandy@gmail.com>
2022-06-05 12:23:32 -0300

    Parametros

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const app = express(); //variavel de aplicação onde recebe as funcionalidades 
 
 //aqui fica uma rota
 app.get("/", function (req, res) {
-  res.send("Hello word");
+  res.send("Hello word");//função send só bode ser enviado uma vez
 });
 
 //aqui fica uma rota
-app.get("/sobre",function(req,res){
-res.send("Bem vindo a meu app")
-})
+app.get("/sobre", function (req, res) {
+  res.send("Bem vindo a meu app");
+});
 
-//configiração de execução 
+app.get("/ola/:nome", function (req, res) {
+  res.send("ola " + req.params.nome);
+});
+
+//configiração de execução
 app.listen(8081, function () {
   console.log("servido rodando na url: http://localhost:8081");
 });
